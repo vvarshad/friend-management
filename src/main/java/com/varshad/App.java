6474445a3873cb7645c415939945df23f2446692
Varshad Venkatraman Ramachandran <vvarshad@gmail.com>
2018-05-20 19:33:10 +0800

    added swagger documentation for the api

--- src/main/java/com/varshad/App.java
+++ src/main/java/com/varshad/App.java
@@ -4,11 +4,16 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.varshad.friend.data.Neo4jDriver;
 import org.jooby.Jooby;
+import org.jooby.Results;
 import org.jooby.json.Jackson;
 import org.jooby.neo4j.Neo4j;
 import org.neo4j.driver.v1.Driver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * @author jooby generator
@@ -45,9 +50,28 @@ public class App extends Jooby {
         post("api/v1/users/subscribe", req -> endPoints.subscribe(req));
         post("api/v1/users/block", req -> endPoints.block(req));
         post("api/v1/users/send-update", req -> endPoints.updateReceipients(req));
+
+        installSwagger("/swagger");
     }
 
     public static void main(final String[] args) {
         run(App::new, args);
+    }
+
+    private String webJarVersion() {
+        InputStream is = App.class.getResourceAsStream("/META-INF/maven/org.webjars/swagger-ui/pom.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties.getProperty("version");
+    }
+
+    private void installSwagger(String path) {
+        assets(path+"/swagger.json", "swagger.json");
+        get(path,() ->  Results.redirect(path+"/ui/index.html?url="+path+"/swagger.json"));
+        assets(path+"/ui/**", "/META-INF/resources/webjars/swagger-ui/"+webJarVersion()+"/{0}");
     }
 }
